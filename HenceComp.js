e2b60a406be9ad1dca4843103621b6b5d42b1b24
Sean Homer <sean.homer@therefore.ca>
2015-07-22 09:13:03 -0400

    - revising structure of objects, split into individual files now

--- HenceComp.js
+++ HenceComp.js
@@ -12,36 +12,6 @@ import _extend from 'lodash/object/extend';
 import _defaults from 'lodash/object/defaults';
 import _keys from 'lodash/object/keys';
 
-let HenceBehaviour = {
-  properties: {
-    props: {
-      type: Object,
-      notify: true
-    }
-  },
-
-  attached() {
-    this.configureProperties(); // will auto fill in this components properties if passed in as an object through the
-  },
-
-  /**
-   * If this element was created on the DOM, and was passed in a props property, use that object to populate this
-   * components properties now, in one fell swoop.
-   */
-    configureProperties() {
-    let self = this;
-    let propOverrides = self.props;
-
-    if (propOverrides) {
-      self.propList.forEach(function (propertyName) {
-        if (propOverrides[propertyName]) {
-          self.set(propertyName, propOverrides[propertyName]);
-        }
-      });
-    }
-  }
-};
-
 /**
  * @constructor
  * @param {Object|*} original The component being defined
@@ -93,7 +63,7 @@ let HenceComp = function (original) {
      *
      * @returns {Polymer} The resulting Polymer instance, able to be leveraged once registered.
      */
-      polymerClass() {
+    polymerClass() {
       if (!_polymerClass && Polymer) {
         _polymerClass = Polymer.Class(this);
       }
@@ -107,7 +77,7 @@ let HenceComp = function (original) {
      *
      * @returns {Boolean} Whether or not the element is registered.
      */
-      registerElement() {
+    registerElement() {
       if (!_polymerRegistered && document && this.polymerClass()) {
         document.registerElement(this.is, this.polymerClass());
         _polymerRegistered = true;
@@ -123,7 +93,7 @@ let HenceComp = function (original) {
      * @param {Object} opts Options for which to configure this new dynamically generated component
      * @returns {Polymer} The resulting created DOM element,
      */
-      createElement(opts = {}) {
+    createElement(opts = {}) {
       let el;
 
       if (this.registerElement()) { // ensure that the element is in fact registered
@@ -140,7 +110,7 @@ let HenceComp = function (original) {
      * @param {Object} target The desired DOM element to append the new component too.
      * @returns {Polymer} The resulting created DOM element,
      */
-      appendElementTo(opts, target = document.body) {
+    appendElementTo(opts, target = document.body) {
       let el = this.createElement(opts);
 
       if (target) {
@@ -154,5 +124,5 @@ let HenceComp = function (original) {
   return comp;
 };
 
-export {HenceBehaviour};
+export * from './HenceBehaviour';
 export default HenceComp;
