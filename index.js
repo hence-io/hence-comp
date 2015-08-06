a339208004ed92152528d32676d5aa568df21c12
Sean Homer <sean.homer@therefore.ca>
2015-08-06 09:41:59 -0400

    - corrections to the use of export default in the core hence-comp lib, which was no longer functional - adjusting the js task options to ensure source maps are visible again

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ export * from './lib/HenceBehaviour';
 export * from './lib/HenceModel';
 export * from './lib/HenceSchema';
 export * from './lib/HenceComp';
+
+import {HenceComp} from './lib/HenceComp';
+export default HenceComp;
