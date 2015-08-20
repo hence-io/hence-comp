315ec052a69720e1169929c336af244f80b44aad
Sean 'Blitz' Homer <sean.homer@therefore.ca>
2015-08-20 15:20:26 -0400

    - revising the usage of the components initialization, now fully extrapolated under the Initialization behaviour, which manages it's class/dom/hook inits without having to expose anything to the child polymer being defined, separating concerns and keeping things from being affected that shouldn't be - exposing the revised behaviour, to be consumable stand alone

--- index.js
+++ index.js
@@ -24,13 +24,17 @@ import {HenceModel} from './lib/model';
 import {HenceSchema} from './lib/schema';
 import {HenceUi} from './lib/ui';
 import {HenceHook} from './lib/hook';
+import {BehaviourInitialization} from './lib/behaviours/initialization';
 
 let Hence = {
   Model: HenceModel,
   Schema: HenceSchema,
   Ui: HenceUi,
-  hook: HenceHook
+  hook: HenceHook,
+  behaviours: {
+    initialization: BehaviourInitialization
+  }
 };
 
-export {HenceModel,HenceSchema,HenceUi,HenceHook}
+export {HenceModel,HenceSchema,HenceUi,HenceHook,BehaviourInitialization}
 export default Hence;
